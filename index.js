7d06520fb42be8ad1c206c846936a8c75dd3eb7d
TurkRagnar <48673608+turkragnar@users.noreply.github.com>
2021-12-18 18:00:37 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+let username = prompt("Username: ");
+if( username.length <= 0 ){
+alert("Please Username..")}
+else{
+	function makeid(length) {
+    var results           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!^+%&/()=?-_*';
+    var charactersLength = characters.length;
+    for ( var a = 0; a < length; a++ ) {
+      results += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return results;
+}
+let passwd = makeid(12);
+	for(var a=0; a<=5; a++){
+	a++;
+document.getElementById('username').value=username;
+document.getElementById('password').value=passwd;
+document.querySelector('.signinbutton').click();
+setTimeout(10000);
+}
+alert('Successful');
+}
